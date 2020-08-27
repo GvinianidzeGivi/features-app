9b73d482a8e1192335681955867763ecf5d7afc0
gvinianidzegivi <gvinianidzegivi@gmail.com>
2020-08-28 00:57:54 +0400

    installed angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModulesModule } from './modules/modules.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    ModulesModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
